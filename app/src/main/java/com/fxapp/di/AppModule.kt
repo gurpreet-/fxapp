77691be177a149b0dbd7d732941bb7e227e9572e
Gurpreet Paul <gurpreet@gurpreet.co>
2024-04-07 12:55:26 +0100

    Create shared prefs

--- app/src/main/java/com/fxapp/di/AppModule.kt
+++ app/src/main/java/com/fxapp/di/AppModule.kt
@@ -2,10 +2,14 @@ package com.fxapp.di
 
 import com.fxapp.libfoundation.di.FoundationModule
 import com.fxapp.libfoundation.wrappers.BuildWrapper
+import com.fxapp.libfoundation.wrappers.SharedPreferencesWrapper
 import com.fxapp.login.di.LoginModule
 import com.fxapp.transfer.di.TransferModule
 import com.fxapp.viewmodel.ConverterViewModel
 import com.fxapp.wrappers.AppBuildWrapper
+import com.fxapp.wrappers.AppSharedPreferencesWrapper
+import com.fxapp.wrappers.AppSharedPreferencesWrapper.Companion.createSharedPrefs
+import org.koin.android.ext.koin.androidContext
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
@@ -13,6 +17,7 @@ import org.koin.dsl.module
 object AppModule {
     val module = module {
         single<BuildWrapper> { AppBuildWrapper() }
+        single<SharedPreferencesWrapper> { AppSharedPreferencesWrapper(createSharedPrefs(androidContext(), get())) }
         viewModel { ConverterViewModel(get()) }
     }
 
--- app/src/main/java/com/fxapp/wrappers/AppSharedPreferencesWrapper.kt
+++ app/src/main/java/com/fxapp/wrappers/AppSharedPreferencesWrapper.kt
@@ -1,7 +1,11 @@
 package com.fxapp.wrappers
 
+import android.content.Context
 import android.content.SharedPreferences
 import androidx.core.content.edit
+import androidx.security.crypto.EncryptedSharedPreferences
+import androidx.security.crypto.MasterKey
+import com.fxapp.libfoundation.wrappers.BuildWrapper
 import com.fxapp.libfoundation.wrappers.SharedPreferencesWrapper
 
 class AppSharedPreferencesWrapper(
@@ -12,4 +16,24 @@ class AppSharedPreferencesWrapper(
     }
 
     override fun getBoolean(key: String, default: Boolean) = sharedPreferences.getBoolean(key, default)
+
+    companion object {
+        fun createSharedPrefs(context: Context, buildWrapper: BuildWrapper): SharedPreferences {
+            return if (buildWrapper.isDebug) {
+                val masterKey: MasterKey = MasterKey.Builder(context)
+                    .setKeyScheme(MasterKey.KeyScheme.AES256_GCM)
+                    .build()
+
+                EncryptedSharedPreferences.create(
+                    context,
+                    "secret_shared_prefs",
+                    masterKey,
+                    EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+                    EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
+                )
+            } else {
+                context.getSharedPreferences("shared_prefs", Context.MODE_PRIVATE)
+            }
+        }
+    }
 }
